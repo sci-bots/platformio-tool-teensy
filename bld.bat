a75bd5c72745828da48190baf0c4428ed897c1d7
Christian Fobel <christian@fobel.net>
2017-08-08 14:19:34 -0400

    Delete `teensy_loader_cli.exe`: force GUI uploader
    Delete `teensy_loader_cli.exe` to force usage of graphical uploader, which
    does not require user to press the `Reset` button.
    
    See [`dropbot.py` issue 19][i19] for more information.
    
    [i19]: https://gitlab.com/sci-bots/dropbot.py/issues/19

--- bld.bat
+++ bld.bat
@@ -9,6 +9,14 @@ REM Extract files from from tar.gz archive.
 mkdir "%NAME%"
 tar xvf %FILENAME% -C "%NAME%"
 if errorlevel 1 exit 1
+REM XXX Delete `teensy_loader_cli.exe` to force usage of graphical uploader,
+REM which does not require user to press the `Reset` button.
+REM
+REM See [`dropbot.py` issue 19][i19] for more information.
+REM
+REM [i19]: https://gitlab.com/sci-bots/dropbot.py/issues/19
+del "%NAME%"\teensy_loader_cli.exe
+if errorlevel 1 exit 1
 REM Copy contents to ${PREFIX}\Library\bin
 xcopy /S /Y /I /Q %NAME% "%PREFIX%"\share\platformio\packages\%NAME%
 if errorlevel 1 exit 1
